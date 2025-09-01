fa113bdd894a8da42287a0b74a9a2f4ae9dc06de
82102 <82102@DESKTOP-8J1OAVB>
2025-09-01 15:28:24 +0900

    eqer

--- FinalProject/src/main/java/com/spring/app/config/SecurityConfig.java
+++ FinalProject/src/main/java/com/spring/app/config/SecurityConfig.java
@@ -64,7 +64,6 @@ public class SecurityConfig
 				          "/WEB-INF/views/**",
 				          // ✅ 스마트에디터 정적 리소스 허용 (여기가 핵심)
 				          "/smarteditor/**",
-				          // (구프로젝트 호환용. 실제로 쓰면 유지, 아니면 지워도 됨)
 					  "/uploads/**",
 				          "/resources/**"
 				         ).permitAll()
